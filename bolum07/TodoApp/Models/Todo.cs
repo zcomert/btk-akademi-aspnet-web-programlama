3209a536c9e8f49ad165dead8c75a3a3b842f1ae
zcomert <zafer.comert@hotmail.com>
2025-09-28 19:29:56 +0300

    DA and FV has been applied.

---
+++ bolum07/TodoApp/Models/Todo.cs
@@ -0,0 +1,12 @@
+﻿namespace TodoApp.Models
+{
+    public class Todo
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public String Title { get; set; } = string.Empty;
+        public String? Description { get; set; }
+        public TodoPriority Priority { get; set; }
+        public DateTime? DueDate { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
