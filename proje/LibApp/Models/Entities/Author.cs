3e0394c70716274fb1f85359a4b8c033c885d0d2
zcomert <zafer.comert@hotmail.com>
2025-11-29 23:38:10 +0300

    chapter-14 has been completed.

---
+++ proje/LibApp/Models/Entities/Author.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibApp.Models.Entities;
+
+public class Author
+{
+    public int AuthorId { get; set; }
+
+    [Required, StringLength(100)]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required, StringLength(100)]
+    public string LastName { get; set; } = string.Empty;
+
+    public DateTime? DateOfBirth { get; set; }
+
+    [StringLength(2000)]
+    public string? Biography { get; set; }
+
+    public ICollection<BookAuthor> BookAuthors { get; set; } = new List<BookAuthor>();
+}
+
---
+++ proje/LibApp/Models/Entities/BookAuthor.cs
@@ -0,0 +1,12 @@
+﻿namespace LibApp.Models.Entities;
+
+public class BookAuthor
+{
+    public int BookId { get; set; }
+
+    public int AuthorId { get; set; }
+
+    public Book? Book { get; set; }
+
+    public Author? Author { get; set; }
+}
